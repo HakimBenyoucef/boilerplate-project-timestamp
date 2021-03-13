6692b020a20afaeb5e42cd86f2525ae165aaeff7
Hakim Benyoucef <benyoucef.hakim@gmail.com>
2021-03-13 18:37:39 +0100

    update server to server timestamp URI

--- server.js
+++ server.js
@@ -8,25 +8,45 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+// your first API endpoint... 
+app.get("/api/timestamp/:time", function(req, res) {
+  const timestamp = req.params.time;
+  const date = getDate(timestamp);
+  if (date != null ) {
+    res.json({ unix: date.getTime(), utc: date.toString()});
+  } else {
+      res.json({ error: "Invalid Date" }); 
+  }
+});
+
+const getDate = (timestamp) => {
+  if (new Date(+timestamp) != "Invalid Date") {
+    return new Date(+timestamp);
+  } else if( new Date(timestamp) != "Invalid Date"){
+    return new Date(timestamp);
+  } else {
+    return null;
+  }
+}
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
